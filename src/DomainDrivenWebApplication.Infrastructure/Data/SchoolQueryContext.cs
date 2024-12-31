8185d0b6a2ef96022b9230d217a09ea49c094117
Ola <olaegby@live.com>
2025-01-01 01:35:35 +0200

    Update SchoolQueryContext.cs

--- src/DomainDrivenWebApplication.Infrastructure/Data/SchoolQueryContext.cs
+++ src/DomainDrivenWebApplication.Infrastructure/Data/SchoolQueryContext.cs
@@ -1,45 +1,44 @@
 ﻿using DomainDrivenWebApplication.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace DomainDrivenWebApplication.Infrastructure.Data
+namespace DomainDrivenWebApplication.Infrastructure.Data;
+
+/// <summary>
+/// Represents the database context for handling read-only operations on schools data.
+/// </summary>
+public sealed class SchoolQueryContext : DbContext
 {
     /// <summary>
-    /// Represents the database context for handling read-only operations on schools data.
+    /// Initializes a new instance of the <see cref="SchoolQueryContext"/> class.
     /// </summary>
-    public sealed class SchoolQueryContext : DbContext
+    /// <param name="options">The DbContext options.</param>
+    public SchoolQueryContext(DbContextOptions<SchoolQueryContext> options) : base(options) { }
+
+    /// <summary>
+    /// Gets or sets the DbSet of schools for read operations.
+    /// </summary>
+    public DbSet<School> Schools { get; set; }
+
+    /// <summary>
+    /// Configures the database model for the School entity.
+    /// </summary>
+    /// <param name="modelBuilder">The model builder instance used to construct the model.</param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SchoolQueryContext"/> class.
-        /// </summary>
-        /// <param name="options">The DbContext options.</param>
-        public SchoolQueryContext(DbContextOptions<SchoolQueryContext> options) : base(options) { }
-
-        /// <summary>
-        /// Gets or sets the DbSet of schools for read operations.
-        /// </summary>
-        public DbSet<School> Schools { get; set; }
-
-        /// <summary>
-        /// Configures the database model for the School entity.
-        /// </summary>
-        /// <param name="modelBuilder">The model builder instance used to construct the model.</param>
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        modelBuilder.Entity<School>(entity =>
         {
-            modelBuilder.Entity<School>(entity =>
+            entity.ToTable("Schools", b => b.IsTemporal(temporalTableBuilder =>
             {
-                entity.ToTable("Schools", b => b.IsTemporal(temporalTableBuilder =>
-                {
-                    temporalTableBuilder.HasPeriodStart("ValidFrom");
-                    temporalTableBuilder.HasPeriodEnd("ValidTo");
-                    temporalTableBuilder.UseHistoryTable("SchoolHistoricalData");
-                }));
+                temporalTableBuilder.HasPeriodStart("ValidFrom");
+                temporalTableBuilder.HasPeriodEnd("ValidTo");
+                temporalTableBuilder.UseHistoryTable("SchoolHistoricalData");
+            }));
 
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.Id).ValueGeneratedOnAdd();
-                entity.Property(e => e.Name).IsRequired();
-                entity.Property(e => e.Address).IsRequired();
-                entity.Property(e => e.PrincipalName).IsRequired();
-            });
-        }
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.Name).IsRequired();
+            entity.Property(e => e.Address).IsRequired();
+            entity.Property(e => e.PrincipalName).IsRequired();
+        });
     }
 }
